2032597acef258ebff36a393fe404f2f66e77b32
RameshRajan-siva <rameshmuthu391@gmail.com>
2023-01-11 02:42:09 +0400

    Greens Address

---
+++ src/org/Company/Employee.java
@@ -0,0 +1,42 @@
+package org.Company;
+
+public class Employee {
+	
+	private void empId(){
+		System.out.println("rames123");
+		
+	}
+	private void empName() {
+		System.out.println("s.p.RameshRajan");
+	}
+	private void empDob() {
+		System.out.println("30/09/1991");
+	}
+	private void empPhone() {
+		System.out.println("6381104477");
+		}
+	private void empEmail() {
+		System.out.println("rameshmuthu391@gmail.com");
+	}	
+	private void empAddress() {
+		System.out.println("yadavar northstreet,idaikal");
+	}
+	public static void main(String[] args) {
+		
+	Employee emp = new Employee();
+	emp.empId();
+	emp.empName();
+	emp.empPhone();
+    emp.empDob();
+	emp.empEmail();
+	emp.empAddress();
+	}
+}
+	
+		
+	
+	
+	
+
+
+
---
+++ src/org/Company/GreensTech.java
@@ -0,0 +1,28 @@
+package org.Company;
+
+public class GreensTech {
+	private void greensomr(){
+System.out.println("greens 0123");
+	}
+private void greensAdayar(){
+	System.out.println("greens2341");
+	}
+	
+	private void greensThambaram(){
+		System.out.println("greens567");
+	}
+	private void greensAnnanagar(){
+	    System.out.println("greensAnnanagar3456");
+	}
+	public static void main(String[] args){
+		
+		GreensTech G = new GreensTech();
+      
+
+		G.greensomr();
+		G.greensAdayar();
+		G.greensThambaram();
+		G.greensAnnanagar();
+	}
+}
+
